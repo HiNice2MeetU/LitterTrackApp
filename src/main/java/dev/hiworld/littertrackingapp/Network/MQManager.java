fb8dffd586f276c7db11c5eeecc27e32f22cdef9
hi_world <jaden250607@gmail.com>
2021-01-15 14:53:57 +0000

    Beginning of MQManager Class

--- src/main/java/dev/hiworld/littertrackingapp/Network/MQManager.java
+++ src/main/java/dev/hiworld/littertrackingapp/Network/MQManager.java
@@ -1,4 +1,26 @@
 package dev.hiworld.littertrackingapp.Network;
 
 public class MQManager {
+    // Globals
+    ArrayList<MQMsg> ListenerList = new ArrayList<MQMsg>();
+
+    // Execute Single Command
+    public synchronized int Execute(MQMsg Input) {
+
+    }
+
+    // Execute List of Commands
+    public synchronized ArrayList<Integer> Execute(MQMsg Input) {
+
+    }
+
+    // Start Network Thread
+    private void StartNet() {
+
+    }
+
+    // Class to be executed on different thread
+    class MQThread implements Runnable {
+
+    }
 }
