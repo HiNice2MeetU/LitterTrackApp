2231e7aedfc25354f9fde6a9fa1825cab48b9113
hi_world <jaden250607@gmail.com>
2021-01-19 11:32:51 +0000

    Got the skeleton of the MQAsyncManager up and running

--- src/main/java/dev/hiworld/littertrackingapp/Network/MQAsyncManager.java
+++ src/main/java/dev/hiworld/littertrackingapp/Network/MQAsyncManager.java
@@ -1,4 +1,50 @@
 package dev.hiworld.littertrackingapp.Network;
 
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+
+import java.util.LinkedList;
+
 public class MQAsyncManager {
+    // Queue List
+    LinkedList <MQListMsg> MsgQueue = new LinkedList<MQListMsg>();
+
+    // Add a msg to the queue
+    public void Add(MQMsg Input, IMqttActionListener SucessCall) {
+
+    }
+
+    // Execute the next msg in the queue
+    public void Next() {
+
+    }
+
+    class MQListMsg {
+        private IMqttActionListener Listener;
+        private MQMsg Msg;
+
+        // Constructor
+        public MQListMsg(IMqttActionListener listener, MQMsg msg) {
+            Listener = listener;
+            Msg = msg;
+        }
+
+        // Getters and Setters
+
+        public IMqttActionListener getListener() {
+            return Listener;
+        }
+
+        public void setListener(IMqttActionListener listener) {
+            Listener = listener;
+        }
+
+        public MQMsg getMsg() {
+            return Msg;
+        }
+
+        public void setMsg(MQMsg msg) {
+            Msg = msg;
+        }
+    }
 }
+
