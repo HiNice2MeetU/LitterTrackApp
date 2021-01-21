ecb05b5744df16cf814ff427fa158ba9a6cd8f02
hi_world <jaden250607@gmail.com>
2021-01-21 13:45:30 +0000

    Got the bottom bar in home activity recoginzing selections and getting the menu item id!

--- src/main/java/dev/hiworld/littertrackingapp/UI/UIThree/HomeActvity.java
+++ src/main/java/dev/hiworld/littertrackingapp/UI/UIThree/HomeActvity.java
@@ -1,8 +1,13 @@
 package dev.hiworld.littertrackingapp.UI.UIThree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import dev.hiworld.littertrackingapp.R;
 
@@ -10,7 +15,26 @@ public class HomeActvity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Init
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_actvity);
+
+        // Get Bottom Bar
+        BottomNavigationView BottomBar = findViewById(R.id.BottomNavi);
+
+        // Set Bottom Bar Callback
+        BottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Switch statement to determine menue item
+                String ItemID = (getResources().getResourceName(item.getItemId()).split("\\/"))[1];
+
+                // Log
+                Log.d("HomeActivity", "Bottom Bar Selected: " + ItemID);
+
+                // Return
+                return true;
+            }
+        });
     }
 }
