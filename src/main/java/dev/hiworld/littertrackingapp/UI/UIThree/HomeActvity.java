f7e74e8d57fda313a79eefa69ac63ccc38ddd602
hi_world <jaden250607@gmail.com>
2021-01-22 11:07:28 +0000

    Now got the bottom bar going to destinations int the nav graph!

--- src/main/java/dev/hiworld/littertrackingapp/UI/UIThree/HomeActvity.java
+++ src/main/java/dev/hiworld/littertrackingapp/UI/UIThree/HomeActvity.java
@@ -2,6 +2,10 @@ package dev.hiworld.littertrackingapp.UI.UIThree;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +14,7 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import dev.hiworld.littertrackingapp.R;
+import dev.hiworld.littertrackingapp.UI.UIThree.HomeFragments.TestFragmentDirections;
 
 public class HomeActvity extends AppCompatActivity {
 
@@ -22,12 +27,38 @@ public class HomeActvity extends AppCompatActivity {
         // Get Bottom Bar
         BottomNavigationView BottomBar = findViewById(R.id.BottomNavi);
 
+
         // Set Bottom Bar Callback
         BottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 // Switch statement to determine menue item
                 String ItemID = (getResources().getResourceName(item.getItemId()).split("\\/"))[1];
+
+                // Get nav
+                NavController NavControl = Navigation.findNavController(findViewById(R.id.Frag));
+
+                String CurrentDest = NavControl.getCurrentDestination().getLabel().toString();
+
+                // Switch Statement to determine what to do
+                switch (ItemID) {
+                    case "Camera":
+                        // If Camera is selected
+                        break;
+                    case "Map":
+                        // If Map is selected
+                        break;
+                    case "Settings":
+                        // If Settings is selected navigate to settings
+                        if (!CurrentDest.equals("fragment_test2")) {
+                            NavDirections Action = TestFragmentDirections.actionTestFragmentToTestFragment2();
+                            NavControl.navigate(Action);
+                            Log.d("HomeActivity", "on destination: " + CurrentDest);
+                        } else {
+                            Log.e("HomeActivity", "Already on destination: " + CurrentDest);
+                        }
+                        break;
+                }
 
                 // Log
                 Log.d("HomeActivity", "Bottom Bar Selected: " + ItemID);
